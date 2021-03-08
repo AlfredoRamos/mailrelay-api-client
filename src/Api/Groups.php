2102c8ab8dd9dded83be7d4487d686b653a00d2c
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-08 14:10:18 -0600

    Implement remaining API endpoints for Groups

--- src/Api/Groups.php
+++ src/Api/Groups.php
@@ -15,7 +15,44 @@ class Groups extends AbstractApi {
 		return $this->get('groups');
 	}
 
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new group.');
+		}
+
+		$required = ['name'];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new group: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('groups', ['json' => $data]);
+	}
+
 	public function getInfo(int $itemId = 0) {
 		return $this->get(sprintf('groups/%d', $itemId));
+	}
+
+	public function deleteGroup(int $itemId = 0) {
+		return $this->delete(sprintf('groups/%d', $itemId));
+	}
+
+	public function updateGroup(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to update group.');
+		}
+
+		return $this->patch(
+			sprintf('groups/%d', $itemId),
+			['json' => $data]
+		);
 	}
 }

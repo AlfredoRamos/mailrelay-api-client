32ef32b6fe184a91ecf34e9a65b77e11303bf13f
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-08 12:45:05 -0600

    Implement remaining API endpoints for Campaigns

--- src/Api/Campaigns.php
+++ src/Api/Campaigns.php
@@ -15,7 +15,99 @@ class Campaigns extends AbstractApi {
 		return $this->get('campaigns');
 	}
 
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new campaign.');
+		}
+
+		$required = [
+			'sender_id',
+			'subject',
+			'html',
+			'target'
+		];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new campaign: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('campaigns', ['json' => $data]);
+	}
+
 	public function getInfo(int $itemId = 0) {
 		return $this->get(sprintf('campaigns/%d', $itemId));
+	}
+
+	public function deleteCampaign(int $itemId = 0) {
+		return $this->delete(sprintf('campaigns/%d', $itemId));
+	}
+
+	public function updateCampaign(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to update campaign.');
+		}
+
+		return $this->patch(
+			sprintf('campaigns/%d', $itemId),
+			['json' => $data]
+		);
+	}
+
+	public function sendCampaign(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to send campaign.');
+		}
+
+		$required = ['target'];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to send campaign: ' . implode(', ', $invalid));
+		}
+
+		return $this->post(
+			sprintf('campaigns/%d/send_all', $itemId),
+			['json' => $data]
+		);
+	}
+
+	public function sendTest(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to send test campaign.');
+		}
+
+		$required = ['test_emails'];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to send test campaign: ' . implode(', ', $invalid));
+		}
+
+		return $this->post(
+			sprintf('campaigns/%d/send_test', $itemId),
+			['json' => $data]
+		);
 	}
 }

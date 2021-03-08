39324a8d9999b07e5d5664c99a168734a9704960
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-08 13:08:54 -0600

    Implement remaining API endpoints for Custom Fields

--- src/Api/CustomFields.php
+++ src/Api/CustomFields.php
@@ -9,3 +9,54 @@
  */
 
 namespace AlfredoRamos\Mailrelay\Api;
+
+class CustomFields extends AbstractApi {
+	public function getList() {
+		return $this->get('custom_fields');
+	}
+
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new custom field.');
+		}
+
+		$required = [
+			'label',
+			'tag_name',
+			'field_type'
+		];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new custom field: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('custom_fields', ['json' => $data]);
+	}
+
+	public function getInfo(int $itemId = 0) {
+		return $this->get(sprintf('custom_fields/%d', $itemId));
+	}
+
+	public function deleteCustomField(int $itemId = 0) {
+		return $this->delete(sprintf('custom_fields/%d', $itemId));
+	}
+
+	public function updateCustomField(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to update custom field.');
+		}
+
+		return $this->patch(
+			sprintf('custom_fields/%d', $itemId),
+			['json' => $data]
+		);
+	}
+}

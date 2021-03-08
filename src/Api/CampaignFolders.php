00fc97d2738379e7a0febd9f04b2e6e596616fdf
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-08 12:54:13 -0600

    Implement remaining API endpoints for Campaign Folders

--- src/Api/CampaignFolders.php
+++ src/Api/CampaignFolders.php
@@ -15,7 +15,40 @@ class CampaignFolders extends AbstractApi {
 		return $this->get('campaign_folders');
 	}
 
+	public function addFolder($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new campaign folder.');
+		}
+
+		$required = ['name'];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new campaign folder: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('campaign_folders', ['json' => $data]);
+	}
+
 	public function getInfo(int $itemId = 0) {
 		return $this->get(sprintf('campaign_folders/%d', $itemId));
+	}
+
+	public function deleteFolder(int $itemId = 0) {
+		return $this->delete(sprintf('campaign_folders/%d', $itemId));
+	}
+
+	public function updateFolder(int $itemId = 0, $data = []) {
+		return $this->patch(s
+			printf('campaign_folders/%d', $itemId),
+			['json' => $data]
+		);
 	}
 }

6508dfdfd8254216bd2244b3839206da5371e051
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-09 20:35:00 -0600

    Implement remaining API endpoints for RSS Campaigns

--- src/Api/RssCampaigns.php
+++ src/Api/RssCampaigns.php
@@ -9,3 +9,62 @@
  */
 
 namespace AlfredoRamos\Mailrelay\Api;
+
+class RssCampaigns extends AbstractApi {
+	public function getList() {
+		return $this->get('rss_campaigns');
+	}
+
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new RSS campaign.');
+		}
+
+		$required = [
+			'sender_id',
+			'subject',
+			'html',
+			'target',
+			'url',
+			'frequency',
+			'number_of_entries'
+		];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new RSS campaign: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('rss_campaigns', ['json' => $data]);
+	}
+
+	public function getInfo(int $itemId = 0) {
+		return $this->get(sprintf('rss_campaigns/%d', $itemId));
+	}
+
+	public function deleteCampaign(int $itemId = 0) {
+		return $this->delete(sprintf('rss_campaigns/%d', $itemId));
+	}
+
+	public function updateCampaign(int $itemId = 0, $data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to update RSS campaign.');
+		}
+
+		return $this->patch(
+			sprintf('rss_campaigns/%d', $itemId),
+			['json' => $data]
+		);
+	}
+
+	public function getProcessed(int $itemId = 0) {
+		return $this->get(sprintf('rss_campaigns/%d/processed_entries', $itemId));
+	}
+}

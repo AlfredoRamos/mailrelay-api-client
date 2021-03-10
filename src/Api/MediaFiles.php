8f7a8466686c98c0c4f71ddcda8070f65acbc083
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-09 20:14:18 -0600

    Implement remaining API endpoints for Media Files

--- src/Api/MediaFiles.php
+++ src/Api/MediaFiles.php
@@ -9,3 +9,71 @@
  */
 
 namespace AlfredoRamos\Mailrelay\Api;
+
+class MediaFiles extends AbstractApi {
+	public function getList() {
+		return $this->get('media_files');
+	}
+
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new media file.');
+		}
+
+		$required = [
+			'file' => ['name', 'content']
+		];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $key => $value) {
+			if (is_numeric($key) && empty($data[$value])) {
+				$invalid[] = $value;
+			}
+
+			if (is_string($key) && is_array($value)) {
+				$inv = [];
+
+				if (empty($data[$key])) {
+					$inv = $required[$key];
+				}
+
+				foreach ($value as $item) {
+					if (empty($data[$key][$item]) && !in_array($item, $inv)) {
+						$inv[] = $item;
+					}
+				}
+
+				if (!empty($inv)) {
+					$invalid[] = sprintf('%s[%s]', $key, implode(',', $inv));
+				}
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new A/B test: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('media_files', ['json' => $data]);
+	}
+
+	public function getInfo(int $itemId = 0) {
+		return $this->get(sprintf('media_files/%d', $itemId));
+	}
+
+	public function deleteMediaFile(int $itemId = 0) {
+		return $this->delete(sprintf('media_files/%d', $itemId));
+	}
+
+	public function moveToTrash(int $itemId = 0) {
+		return $this->patch(sprintf('media_files/%d/move_to_trash', $itemId));
+	}
+
+	public function restore(int $itemId = 0) {
+		return $this->patch(sprintf('media_files/%d/restore', $itemId));
+	}
+
+	public function trashed() {
+		return $this->get('media_files/trashed');
+	}
+}

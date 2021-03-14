0ac658d6f90e4d0d9813090d98bd00bef564dc38
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-13 18:57:08 -0600

    Implement remaining API endpoints for Unsubscribe Events

--- src/Api/UnsubscribeEvents.php
+++ src/Api/UnsubscribeEvents.php
@@ -9,3 +9,39 @@
  */
 
 namespace AlfredoRamos\Mailrelay\Api;
+
+class UnsubscribeEvents extends AbstractApi {
+	public function getList() {
+		return $this->get('unsubscribe_events');
+	}
+
+	public function addNew($data = []) {
+		if (!is_array($data)) {
+			throw new \InvalidArgumentException('Invalid data to add a new unsubscribe event.');
+		}
+
+		$required = ['sent_email_id'];
+		$invalid = [];
+
+		// TODO: Validate allowed values of required fields
+		foreach ($required as $item) {
+			if (empty($data[$item])) {
+				$invalid[] = $item;
+			}
+		}
+
+		if (!empty($invalid)) {
+			throw new \InvalidArgumentException('Missing required data to add a new unsubscribe event: ' . implode(', ', $invalid));
+		}
+
+		return $this->post('unsubscribe_events', ['json' => $data]);
+	}
+
+	public function getInfo(int $itemId = 0) {
+		return $this->get(sprintf('unsubscribe_events/%d', $itemId));
+	}
+
+	public function deleteEvent(int $itemId = 0) {
+		return $this->delete(sprintf('unsubscribe_events/%d', $itemId));
+	}
+}

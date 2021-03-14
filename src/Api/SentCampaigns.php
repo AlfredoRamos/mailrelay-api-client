3a7f546dd69d04eaaeeabfbaa58d32a5cfb69387
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-03-13 18:46:04 -0600

    Implement remaining API endpoints for Sent Campaigns

--- src/Api/SentCampaigns.php
+++ src/Api/SentCampaigns.php
@@ -9,3 +9,41 @@
  */
 
 namespace AlfredoRamos\Mailrelay\Api;
+
+class SentCampaigns extends AbstractApi {
+	public function getList() {
+		return $this->get('sent_campaigns');
+	}
+
+	public function getInfo(int $itemId = 0) {
+		return $this->get(sprintf('sent_campaigns/%d', $itemId));
+	}
+
+	public function cancelCampaign(int $itemId = 0) {
+		return $this->patch(sprintf('sent_campaigns/%d/cancel', $itemId));
+	}
+
+	public function getClicks(int $itemId = 0) {
+		return $this->get(sprintf('sent_campaigns/%d/clicks', $itemId));
+	}
+
+	public function getImpressions(int $itemId = 0) {
+		return $this->get(sprintf('sent_campaigns/%d/impressions', $itemId));
+	}
+
+	public function pauseCampaign(int $itemId = 0) {
+		return $this->patch(sprintf('sent_campaigns/%d/pause', $itemId));
+	}
+
+	public function resumeCampaign(int $itemId = 0) {
+		return $this->patch(sprintf('sent_campaigns/%d/resume', $itemId));
+	}
+
+	public function getSentEmails(int $itemId = 0) {
+		return $this->get(sprintf('sent_campaigns/%d/sent_emails', $itemId));
+	}
+
+	public function getUnsubscribeEvents(int $itemId = 0) {
+		return $this->get(sprintf('sent_campaigns/%d/unsubscribe_events', $itemId));
+	}
+}
